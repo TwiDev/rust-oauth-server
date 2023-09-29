b93c0238b3cf00c2f63110385f6a306c1fb7cbf8
TwiDev <twidev5@gmail.com>
2023-09-29 21:32:31 +0200

    update

--- src/test.rs
+++ src/test.rs
@@ -1,1 +1,4 @@
 use std::string::ToString;
+
+// /oauth/authorize/ -> {"client_id":6,"scopes":10}
+// /oauth/token/ -> {"client_id":6,"client_secret":"aysMl39jJPcr1_1FJo1Ma","grant_type":"ALL","code":"xgYEsJMQHb2eBZK8WTjHA"}
